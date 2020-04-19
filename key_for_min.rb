ec2f18b5e52058c0b476cc9bf079e1f2bc4a4c91
Evan Burton <evan@heyevan.com>
2020-04-19 17:17:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,6 @@
 
 def key_for_min_value(name_hash)
+<<<<<<< HEAD
 
 min_key = nil
 min_value = nil
@@ -11,3 +12,8 @@ name_hash.each do |k, v|
 end
 min_key
 end
+=======
+key_for_min_value(name_hash).collect do |item, value|
+  end
+end
+>>>>>>> 940e6d6972b2ccd12650ae6edb70889bde091a93
